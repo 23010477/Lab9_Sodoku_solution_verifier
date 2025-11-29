2b49850034d000e3d293c6847efec493a9e896bd
nader8757634 <naderking9968@gmail.com>
2025-11-29 18:55:56 +0200

    9 rows Threads

--- src/main/java/Main/Mode27.java
+++ src/main/java/Main/Mode27.java
@@ -1,7 +1,50 @@
 package Main;
 
+import java.util.ArrayList;
+import java.util.List;
+import Backend.BoardMethods.*;
+
+import Backend.BoardMethods.SudokuBoard;
 import Backend.BoardMethods.SudokuBoardService;
 
 public class Mode27  extends SudokuBoardService implements Runnable{
+	private SudokuBoard board;
+    public Mode27(SudokuBoard board ) {
+        this.board = board;
+    }
+    
+    public List<String> validateBoard() throws InterruptedException {
+    	System.out.println("Starting Mode3 validation...");
+
+        List<String> errors = new ArrayList<>();
+        List<Thread> threads = new ArrayList<>();
+        
+        //9 rows threads 
+        for(int i=0; i<9; i++) {
+        	int index =i;
+        	Thread r = new Thread(() ->{
+        		if(!rowValidation(i)) {
+        			  synchronized (errors) {
+        				  errors.add("Row " + (i + 1) + " invalid");
+        			  }
+        			
+        			
+        		}
+        		
+        	});
+        	
+            threads.add(r);
+        	
+        }
+		return errors;
+        
+        
+    }
+    
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
