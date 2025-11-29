88fe3410fa2332ae306a3ab6716d87be3a26ac74
nader8757634 <naderking9968@gmail.com>
2025-11-29 20:36:30 +0200

    finished , test

--- src/main/java/Main/Mode27.java
+++ src/main/java/Main/Mode27.java
@@ -14,7 +14,7 @@ public class Mode27  extends SudokuBoardService implements Runnable{
     }
     
     public List<String> validateBoard() throws InterruptedException {
-    	System.out.println("Starting Mode3 validation...");
+    	System.out.println("Starting Mode27 validation...");
 
         List<String> errors = new ArrayList<>();
         List<Thread> threads = new ArrayList<>();
@@ -88,8 +88,33 @@ public class Mode27  extends SudokuBoardService implements Runnable{
     
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		try {
+            List<String> errors = validateBoard();
+            for (String error : errors) {
+                System.out.println(error);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 	}
 	
+	public static void main(String[] args) {
+        String filePath = "src/main/java/Backend/BoardMethods/test.txt";
+        SudokuBoard board = SudokuBoard.getInstance(filePath);
+
+        Mode27 mode27 = new Mode27(board);
+        mode27.run();
+        System.out.println("Mode 3 completed");
+        try{
+            if(mode27.validateBoard().isEmpty()) {
+                System.out.println("Board is valid");
+            } else {
+                System.out.println("Board is invalid");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+	}
 }
+
