3fcc257cb9f19c800ea9b1cc3254831afb44a061
Mohamed <mohamedwalid.elkholy@gmail.com>
2025-11-30 02:31:20 +0200

    added constructor and attributes

--- src/main/java/Main/Mode0.java
+++ src/main/java/Main/Mode0.java
@@ -1,5 +1,11 @@
 package Main;
 
+import Backend.BoardMethods.SudokuBoard;
+
 public class Mode0 {
-    
+    private SudokuBoard board;
+    public Mode0(SudokuBoard board){
+        this.board=board;
+    }
+
 }
