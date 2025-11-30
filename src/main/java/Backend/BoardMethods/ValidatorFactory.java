1f7752874af06934a36607ed1110b29b26ed5998
Mohamed <mohamedwalid.elkholy@gmail.com>
2025-11-30 02:28:08 +0200

    uncommented case 0

--- src/main/java/Backend/BoardMethods/ValidatorFactory.java
+++ src/main/java/Backend/BoardMethods/ValidatorFactory.java
@@ -7,7 +7,7 @@ public class ValidatorFactory {
     public static Runnable createValidator(SudokuBoard board, int mode) {
 
         return switch (mode) {
-            // case 0 -> new Mode0(board);
+            case 0 -> new Mode0(board);
             case 3 -> new Mode3(board);
             // case 27 -> new Mode27(board);
             default -> throw new IllegalArgumentException("Invalid mode");
